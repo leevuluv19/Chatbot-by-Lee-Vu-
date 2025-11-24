ee848e51e86571c5f545fe070b4497c28da695cd
leevuluv19 <6mcdr98564@privaterelay.appleid.com>
2025-11-24 15:42:16 +0700

    Add files via upload

--- chatbot_new.py
+++ chatbot_new.py
@@ -49,7 +49,7 @@ st.markdown("""
         mask-composite: exclude;
         
         /* ĐÂY LÀ CHÌA KHÓA: Làm nhòe mạnh ngay từ lớp chính */
-        filter: blur(20px); 
+        filter: blur(15px); 
         opacity: 0.8;
     }
     
@@ -77,7 +77,7 @@ st.markdown("""
 
         /* Blur cực mạnh để tạo cảm giác như khói/sương */
         filter: blur(50px); 
-        opacity: 0.7;
+        opacity: 0.5;
     }
     /* ẨN GIAO DIỆN CŨ */
     #MainMenu, footer, header {visibility: hidden;}
